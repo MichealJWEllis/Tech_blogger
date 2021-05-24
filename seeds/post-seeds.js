9c17c5d742f29a8c8b087167d374632ff57e07ff
Micheal Ellis <76069321+MichealJWEllis@users.noreply.github.com>
2021-05-24 10:55:25 -0400

    added info for seeds files/api files

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,14 +3,20 @@ const { Post } = require('../models');
 const postdata = [
     {
         title: 'Why MVC is so important',
+        post_url: 'https://www.c-sharpcorner.com/article/what-is-mvc-and-why-we-use-mvc/',
+        context: 'A description of the first post',
         user_id: 1
     },
     {
         title: 'Authentication vs. Authorization',
+        post_url: 'https://www.okta.com/identity-101/authentication-vs-authorization/',
+        context: 'A description of the second post',
         user_id: 2
     },
     {
         title: 'Object-Relational Mapping',
+        post_url: 'https://circle.visual-paradigm.com/docs/database-design-engineering/programmers-guide/what-is-object-relational-mapping-orm/',
+        context: 'A descrition of the third post',
         user_id: 3
     }
 ];
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,17 +4,17 @@ const { User } = require('../models');
 const userData = [
     {
         username: 'blogger1',
-        email: 'blogger1@me.com',
+        email: 'blogger1@blah.com',
         password: 'password111'
     },
     {
         username: 'blogger2',
-        email: 'blogger2@me.com',
+        email: 'blogger2@asdf.com',
         password: 'password222'
     },
     {
         username: 'blogger3',
-        email: 'blogger3@me.com',
+        email: 'blogger3@asdfg.com',
         password: 'password333'
     }
 ]
