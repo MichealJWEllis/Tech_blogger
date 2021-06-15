80514064d60dee416773117b39b1c01649e3a546
Micheal Ellis <76069321+MichealJWEllis@users.noreply.github.com>
2021-06-15 16:00:28 -0400

    changes

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,14 @@
-let sequelize;
-
+const Sequelize = require('sequelize');
+// .env created for use of config
 require('dotenv').config();
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+// create connection to local db or connection with JAWSDB
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
     });
-}
 
 module.exports = sequelize;
